caf0d789fdf8b96e528e327de9c875c04f3234a4
HewJhong <jinhong18hogrider@gmail.com>
2020-11-29 22:30:58 +0800

    new changepage.js

---
+++ changepage.js
@@ -0,0 +1,21 @@
+function homepage() {
+    $("#main-content").load("adminhome.php");
+}
+function quizlistpage() {
+    $("#main-content").load("adminkuizlist.php");
+}
+function quizpage() {
+    $("#main-content").load("adminkuiz.php");
+}
+function resultpage() {
+    $("#main-content").load("studentresult.php");
+}
+function hometitle() {
+    $("#title").load("hometitle.txt");
+}
+function kuiztitle() {
+    $("#title").load("kuiztitle.php");
+}
+function destroysession() {
+    window.location.href = "./logKeluar.php";
+}
