6dff3ee5e73583e863dbb4398e2524af0e8154da
HewJhong <38809009+HewJhong@users.noreply.github.com>
2021-01-16 17:35:15 +0800

    Delete extra lines

--- kuizlistdb.php
+++ kuizlistdb.php
@@ -1,7 +1,3 @@
-<html>
-<head>
-</head>
-</html>
 <?php
 $servername = "localhost";
 $username = "root";
@@ -17,60 +13,71 @@ if ($conn->connect_error) {
 
 $soalprefix = "S";
 $counter = 0;
-$sql1 = mysqli_query($conn, "select * from topik");
+$sql1 = mysqli_query($conn, "SELECT * FROM topik");
 $soallist = array();
+$gotsoal = array();
 
-while ($row1 = mysqli_fetch_array($sql1)){
+while ($row1 = mysqli_fetch_assoc($sql1)){
   $counter++;
   $soal = [];
   $soallist = [];
   $idtopik = $row1['idtopik'];
   $sql2 = mysqli_query($conn, "SELECT * FROM soalan WHERE (idtopik = '$idtopik') ORDER BY LENGTH (idsoal) ASC, idsoal ASC");
-  $nosoal = mysqli_num_rows($sql2);
-  $nosoal = $nosoal / 4;
-  if ($nosoal >= 1){
+  $numsoal = mysqli_num_rows($sql2);
+  $numsoal = $numsoal / 4;
+  if ($numsoal >= 1){
     echo "<button class='collapsible btn'>".$row1['topik']."</button>";
     echo "<div class='content'>";
-  }
-  while ($fetchsoal = mysqli_fetch_array($sql2)){
-    $resultsoal = $fetchsoal['soal'];
-    $resultidsoal = $fetchsoal['idsoal'];
-    array_push($soallist, $resultsoal);
-  }
-  $soal = array_unique($soallist);
-  $soalcount = count($soal);
-  foreach ($soal as $value){
-    $sql3 = mysqli_query($conn, "SELECT * FROM soalan WHERE (soal = '$value')");
-    $soalresult = mysqli_fetch_array($sql3);
-    $nosoal = $soalresult['nosoal'];
-    echo "<div class='row'>";
-    echo "<h5 class='contenttext col-sm'>".$value."</h5>";
-    echo "<a class='col-sm-1 btn btn-primary edit-soal-btn' id='".$nosoal."'>Ubah</a>";
-    // echo "<a class='col-sm-1 btn btn-primary edit-soal-btn' id='".$nosoal."' data-toggle='modal' data-target='#edit-soal'>Ubah</a>";
-    echo "<a title='Hapuskan' class='col-sm-1 btn btn-danger delete-soal-btn' id='".$nosoal."' data-toggle='modal' data-target='#confirmationmodal'>X</a>";
-    // echo "<a title='Hapuskan' class='col-sm-1 btn btn-danger delete-soal-btn' id='".$nosoal."'>X</a>";
-    echo "</div>";
-      }
+    array_push($gotsoal, "1");
+
+    while ($fetchsoal = mysqli_fetch_array($sql2)){
+      $resultsoal = $fetchsoal['soal'];
+      $resultidsoal = $fetchsoal['idsoal'];
+      array_push($soallist, $resultsoal);
+    }
+    $soal = array_unique($soallist);
+    $soalcount = count($soal);
+    foreach ($soal as $value){
+      $sql3 = mysqli_query($conn, "SELECT * FROM soalan WHERE (soal = '$value')");
+      $soalresult = mysqli_fetch_array($sql3);
+      $nosoal = $soalresult['nosoal'];
+      echo "<div class='row'>";
+      echo "<h5 class='contenttext col-sm'>".$value."</h5>";
+      echo "<a class='col-sm-1 btn btn-primary edit-soal-btn' id='".$nosoal."'>Ubah</a>";
+      // echo "<a class='col-sm-1 btn btn-primary edit-soal-btn' id='".$nosoal."' data-toggle='modal' data-target='#edit-soal'>Ubah</a>";
+      echo "<a title='Hapuskan' class='col-sm-1 btn btn-danger delete-soal-btn' id='".$nosoal."' data-toggle='modal' data-target='#confirmationmodal'>X</a>";
+      // echo "<a title='Hapuskan' class='col-sm-1 btn btn-danger delete-soal-btn' id='".$nosoal."'>X</a>";
       echo "</div>";
     }
-    echo " 
-    <script>
-    var coll = document.getElementsByClassName('collapsible');
-    var i;
+  } 
+  else {
+    array_push($gotsoal, "0");
+  }
+    echo "</div>";
+}
 
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight){
-          content.style.maxHeight = null;
-        } else {
-          content.style.maxHeight = content.scrollHeight + 'px';
-        } 
-      });
-    }
-    </script>
-    ";
+if (!in_array("1", $gotsoal)) {
+  echo "<h3 class='text-center'>Tiada Soalan...<h3>";
+}
+
+  echo " 
+  <script>
+  var coll = document.getElementsByClassName('collapsible');
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function() {
+      this.classList.toggle('active');
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + 'px';
+      } 
+    });
+  }
+  </script>
+  ";
 $conn->close();
 
 ?>
