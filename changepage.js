bd61d932fdbff6c65665524a4d783719caa99f96
HewJhong <38809009+HewJhong@users.noreply.github.com>
2021-01-16 17:34:56 +0800

    Add student change page functions

--- changepage.js
+++ changepage.js
@@ -16,6 +16,24 @@ function hometitle() {
 function kuiztitle() {
     $("#title").load("kuiztitle.php");
 }
+
+function studenthome() {
+    $("#main-content").load("studenthome.php");
+}
+function studentresult() {
+    $("#main-content").load("studentresult.php");
+}
+function studentkuizlist() {
+    $("#main-content").load("studentkuizlist.php");
+}
+function studentjawabkuiz() {
+    $("#main-content").load("studentjawabkuiz.php");
+}
+
+function refreshpage() {
+    $("#main-content").load("adminkuizlist.php");
+
+}
 function destroysession() {
     window.location.href = "./logKeluar.php";
 }
