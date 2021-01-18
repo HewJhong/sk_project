e6c20ca8b2e5f3ac82ca22a4afe2da628de5c518
HewJhong <38809009+HewJhong@users.noreply.github.com>
2021-01-18 12:45:06 +0800

    Add model

--- studentjawabkuiz.php
+++ studentjawabkuiz.php
@@ -14,9 +14,11 @@
   crossorigin="anonymous"></script>
   <script src="changepage.js"></script>
   </head>
-<div class="quiz-form-div" id="jawabkuizform">
 <script>
   $(document).ready(function() {
+    $('#jawabkuizform').submit(function(){
+        return false;
+    });
     $.ajax({
       type: "GET",
       url: "studentjawabkuizdb.php",
@@ -26,17 +28,41 @@
       }
     }); 
   });
-  $(document).on('click', '#submit-jawab-kuiz-btn', function() {
-    var myData = jQuery('#jawabkuizform').serialize();
+  $(document).off().on('click', '#submit-jawab-kuiz-btn', function() {
+    var count = $('.soalcontainer').length
+    var obj = $("#jawabkuizform").serializeArray();
     $.ajax({
       url: 'studentkuizprocessdb.php',
       type: 'POST',
-      data: myData,
+      data: {data: obj, count: count},
       success: function(result){
-        console.log(result);
+        if (result == "errorNoAns") {
+          $('#warningModel').modal('show');
+        } else {
+          alert(result);
+          studentresult();
+        }
       }
-    })
-  })
+    });
+  });
 </script>
+<div class="modal fade" id="warningModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Warning</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Sila jawab semua soalan...
+      </div>
+    </div>
+  </div>
+</div>
+<div class="quiz-form-div">
+<form id="jawabkuizform">
+</form>
 </div>
 </html>
