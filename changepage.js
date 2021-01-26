63a8aec6aadf3ba53619631f5dc6f31c48f40c3a
HewJhong <38809009+HewJhong@users.noreply.github.com>
2021-01-26 22:22:33 +0800

    Add url parameter

--- changepage.js
+++ changepage.js
@@ -1,13 +1,17 @@
 function homepage() {
+    window.history.pushState("string", "home", "adminmain.php?page=adminhome");
     $("#main-content").load("adminhome.php");
 }
 function quizlistpage() {
+    window.history.pushState("string", "kuizlist", "adminmain.php?page=adminkuizlist");
     $("#main-content").load("adminkuizlist.php");
 }
 function quizpage() {
+    window.history.pushState("string", "kuiz", "adminmain.php?page=adminkuiz");
     $("#main-content").load("adminkuiz.php");
 }
 function resultpage() {
+    window.history.pushState("string", "result", "adminmain.php?page=adminkeputusan");
     $("#main-content").load("adminresultlist.php");
 }
 function hometitle() {
