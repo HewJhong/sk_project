26e19c22d15f72ea5233f5dae5df28c698671d8e
Cheah Zixu <cheah3838@gmail.com>
2021-01-27 09:06:11 +0800

    Fix content switch

--- adminmain.php
+++ adminmain.php
@@ -1,21 +1,4 @@
-<?php require_once 'authController.php';
-if (!isset($_SESSION['nop'])) {
-    header ("Location: login.php");
-    exit();
-}
-else if ($_GET['page'] == "adminhome"){
-    echo "<script>homepage();</script>";
-}
-else if ($_GET['page'] == "adminkeputusan"){
-    echo "<script>resultpage();</script>";
-}
-else if ($_GET['page'] == "adminkuizlist"){
-    echo "<script>quizlistpage();</script>";
-}
-else if ($_GET['page'] == "adminkuiz"){
-    echo "<script>quizpage();</script>";
-}
-?>
+<?php require_once 'authController.php';?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,10 +7,6 @@ else if ($_GET['page'] == "adminkuiz"){
     <link href='https://fonts.googleapis.com/css?family=Galada' rel='stylesheet'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-    crossorigin="anonymous"></script>
-    <script src="changepage.js"></script>
     <!-- <div id="navbar" class="header">
         <button style="font-family: 'Galada';font-size: 40px;"class="top-title text-center navbar-home" onclick="homepage()">Sistem Penilaian Kuiz Matematik</button>
         <button type="text" class="top-title text-center navbar-btn-logout" onclick="destroysession()">Log Out</button>
@@ -66,19 +45,41 @@ else if ($_GET['page'] == "adminkuiz"){
             </div>
             </div>
     <?php endif ?>
+</head>
+<body style="overflow: hidden;">
+    <div id="main-content"  style="overflow-x: hidden; overflow-y: scroll; height: 89%;"></div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+    crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" 
     integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" 
     crossorigin="anonymous"></script>
+    <script src="changepage.js"></script>
     <script>
     $(document).ready(function(){
-        $("#main-content").load("adminhome.php");
+        // $("#main-content").load("adminhome.php");
     });
     $(document).on('click', '#dropdown-btn', function() {
         $('#kuizdropdownbtn').dropdown('toggle');
     })
     </script> 
-</head>
-<body style="overflow: hidden;">
-    <div id="main-content"  style="overflow-x: hidden; overflow-y: scroll; height: 89%;"></div>
+    <?php 
+        if (!isset($_SESSION['nop'])) {
+            header ("Location: login.php");
+            exit();
+        }
+        else if ($_GET['page'] == "adminhome"){
+            echo "<script>homepage();</script>";
+        }
+        else if ($_GET['page'] == "adminkeputusan"){
+            echo "<script>resultpage();</script>";
+        }
+        else if ($_GET['page'] == "adminkuizlist"){
+            echo "<script>quizlistpage();</script>";
+        }
+        else if ($_GET['page'] == "adminkuiz"){
+            echo "<script>quizpage();</script>";
+        }
+    ?>
 </body>
 </html>
