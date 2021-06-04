e15c59d2202f1e9f3037120507c33cdc2ef61b4a
HewJhong <38809009+HewJhong@users.noreply.github.com>
2021-06-04 15:13:28 +0800

    Title icon testing

---
+++ icontesting.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<head>
+<!-- scripts -->
+    <meta charset="UTF-8">
+    <title id="title">Home</title>
+    <link href='https://fonts.googleapis.com/css?family=Galada' rel='stylesheet'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+    crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" 
+    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" 
+    crossorigin="anonymous"></script>
+    <script src="changepage.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" 
+    integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" 
+    crossorigin="anonymous">
+    <script src="auto-tables.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+    <script>
+        function addDarkmodeWidget() {
+            new Darkmode().showWidget();
+        }
+        window.addEventListener('load', addDarkmodeWidget);
+    </script>
+    <script src='functions.js'></script>
+    <script src='fontSizeChanger.js'></script>
+<!-- scripts -->
+    <title>Document</title>
+    <link rel="shortcut icon" type="image/jpg" href="/logo.jpg"/>
+    <link rel="icon" type="image/jpg" href="/logo.jpg"/>
+</head>
+<body>
+    
+</body>
+</html>
